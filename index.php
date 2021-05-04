d79bffbdc1c672bc1b612ef9fe3fb39e71992ba2
Johao Lopez <johao.lopz.m@gmail.com>
2021-05-04 00:38:34 -0500

    Extraccion input.value en JS y uso de propiedad parseInt

--- index.php
+++ index.php
@@ -28,9 +28,38 @@
 		echo "Hola, $a. Esto es una prueba!!!";
 		?>
 	</h1><br>
-	<h2 class=texto>EL CUENTACUENTOS</h2><br>
-	<div class='texto'><button id='testButton'>Vamos!</button></div><br><br>
-	<div id='respuesta' class='texto'></div>
+	<h2 class="texto">EL CUENTACUENTOS</h2><br>
+	<div class="texto"><button id='testButton'>Vamos!</button></div><br><br>
+	<div id='respuesta' class='texto'></div><br>
+	<h1 class="texto">Uso del parseInt (Convertir caracteres a números enteros)</h1><br>
+	<div class='texto'>
+	<table id="tabla">
+		<tbody>
+		<tr>
+		<td>Número A: </td>
+		<td><input type="text" name="numA" value=12 disabled></td>
+		</tr>
+		<tr>
+		<td>Número B: </td>
+		<td><input type="text" name="numB" value=13 disabled></td>
+		</tr><tr></tr>
+		<tr>
+		<td><button id='testButton2'>SUMAR A+B</button></td>
+		<td><input id="respuesta2" type="text" disabled></td>
+		</tr>
+		<tr>
+		<td><button id='testButton3'>SUMA (parseInt)</button></td>
+		<td><input id="respuesta3" type="text" disabled></td>
+		</tr>
+		<tr>
+		<td></td>
+		<td>
+		<span id="mensajeOculto"></span>
+		</td>
+		</tr>
+		</tbody>
+	</table>
+	</div>
 </body>
 <script type="text/javascript" src="js/pruebasJs.js"></script>
 </html>
--- js/pruebasJs.js
+++ js/pruebasJs.js
@@ -1,8 +1,18 @@
 //Obtenemos el boton del archivo index.php
 var button = document.getElementById('testButton');
+//Segundo y tercer button
+var button2 = document.getElementById('testButton2');
+var button3 = document.getElementById('testButton3');
+
+//Obtenes los valores de los input
+var numA = document.getElementsByName("numA")[0].value;
+var numB = document.getElementsByName('numB')[0].value;
 
 // Registramos el evento click
 button.addEventListener('click', Evento_Click);
+//Registro del segundo y tercer button
+button2.addEventListener('click', Evento_Click2);
+button3.addEventListener('click', Evento_Click3);
 
 //
 function Evento_Click() {
@@ -17,6 +27,22 @@ console.log("Cuando te atreves a "+verbo+". El "+sustan+" se vuelve más "+adjet
 document.getElementById('respuesta').innerHTML += "<p>Cuando te atreves a "+verbo+". "+sustan+" se vuelve más "+adjeti+".</p>";
 //document.write("<p>Cuando te atreves a "+verbo+". "+sustan+" se vuelve más "+adjeti+".</p>");
 }
+
+//Segundo evento click
+function Evento_Click2() {
+var sumaString = numA+numB;
+document.getElementById('respuesta2').value = sumaString;
+document.getElementById('mensajeOculto').innerHTML = 'Error, 12 + 13 no es "1213"';
+document.getElementById('respuesta3').value = "";
+}
+
+//Tercer evento click
+function Evento_Click3() {
+    var sumaNumerica = parseInt(numA) + parseInt(numB);
+    document.getElementById('respuesta3').value = sumaNumerica;
+    document.getElementById('mensajeOculto').innerHTML = 'Correcto, el resultado es: "25"';
+    document.getElementById('respuesta2').value = "";
+    }
 
 var mensaje = "Hola como estas? ";
 mensaje +=  "Nos alegra que visites la pagina de nuestra organización. ";
